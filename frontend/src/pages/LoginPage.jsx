c6dadb404d79e2905e30c042d589d9c4c28acee6
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-03-04 15:26:50 +0800

    feat: Added simple login page with its css

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "../styles/LoginPage.css";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={handleLogin}>
+          <div className="form-group">
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button type="submit">Login</button>
+        </form>
+
+        <div>
+          <a>Register</a>
+          <a>Forgot Password?</a>
+        </div>
+      </div>
+    </div>
+  );
+}
