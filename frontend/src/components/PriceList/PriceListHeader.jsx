450501492836372615f6e536e21e2891c64b89d2
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-03-06 15:13:59 +0800

    feat: Added more styling to header and added avatar image

--- frontend/src/components/PriceList/PriceListHeader.jsx
+++ frontend/src/components/PriceList/PriceListHeader.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 import LanguageSelect from "../LanguageSelect";
 import styles from "../../styles/PriceList/PriceListHeader.module.css";
+import avatar from "../../assets/avatar.png";
 
 export default function PriceListHeader() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       <header className={styles.navbar}>
         <div className="navbar-inner">
           <div className={styles["navbar-left"]}>
-            <button className={`hamburger-btn`}>
+            <button
+              className={`${styles["hamburger"]} ${styles[menuOpen ? "open" : ""]}`}
+            >
               <span />
               <span />
               <span />
             </button>
 
-            <div className="avatar">AL</div>
-            <div className={styles["user-info"]}>
-              <span>Alexandra Lee</span>
-              <span>Product Manager</span>
+            <div className={styles.avatar}>
+              <img className={styles["avatar-image"]} src={avatar} />
+            </div>
+            <div className={styles["user-info-container"]}>
+              <span className={styles.username}>Alexandra Lee</span>
+              <span className={styles.designation}>Product Manager</span>
             </div>
           </div>
           <div className="navbar-right">
