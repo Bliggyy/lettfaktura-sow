36f3fd6480ed4e98051a52419b092ea905a67884
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-03-09 14:29:08 +0800

    feat: Added JWT token when loggin in

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,8 @@ const authRouter = require("../src/routes/authRoute");
 const app = express();
 const port = process.env.APP_PORT || 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "Success",
--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,7 +1,29 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const generateToken = (payload) => {
+  const secret = process.env.JWT_SECRET_KEY || "secret-key-9582-!!#$%";
+  const expiry = process.env.JWT_DEFAULT_EXPIRY || "9h";
+
+  return jwt.sign(payload, secret, { expiresIn: expiry });
+};
+
 const login = (req, res, next) => {
-  res.json({
-    status: "Success",
-    message: "User has successfully logged in",
+  const body = req.body;
+
+  if (body.username !== "admin" || body.password !== "password") {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Incorrect username and/or password",
+    });
+  }
+
+  const token = generateToken({
+    id: body.username,
+  });
+
+  return res.status(200).json({
+    token: token,
   });
 };
 
