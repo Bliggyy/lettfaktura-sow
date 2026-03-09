1dea5b0a6b159c73e8c6a82cee9707046f0553f1
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-03-09 14:47:32 +0800

    feat: Added auth provider and auth context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@ import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import PriceList from "./pages/PriceList";
 import { Routes, Route } from "react-router";
+import ProtectedRoute from "./components/ProtectedRoutes";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/pricelist" element={<PriceList />} />
+        <ProtectedRoute>
+          <Route path="/pricelist" element={<PriceList />} />
+        </ProtectedRoute>
       </Routes>
     </>
   );
