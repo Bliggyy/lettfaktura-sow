12eca6c9d9d4802710e6e0ec61adf0fb9dfe515f
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-03-09 13:36:36 +0800

    feat: Added migration for pricelist

---
+++ backend/db/migrations/20260309052755-create-pricelist.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Pricelists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      articleNo: {
+        type: Sequelize.UUID
+      },
+      product: {
+        type: Sequelize.STRING
+      },
+      inPrice: {
+        type: Sequelize.DOUBLE
+      },
+      price: {
+        type: Sequelize.DOUBLE
+      },
+      unit: {
+        type: Sequelize.STRING
+      },
+      inStock: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Pricelists');
+  }
+};
