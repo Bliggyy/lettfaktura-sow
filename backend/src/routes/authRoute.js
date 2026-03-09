4faa559fa3d9840db8fa5a0e199fa5ba722290f4
Bligh Ignacio <73883691+Bliggyy@users.noreply.github.com>
2026-03-09 15:10:58 +0800

    feat: Added authenticate to logout route

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,8 +1,9 @@
+const authenticate = require("../middleware/authenticate");
 const { login, logout } = require("../controller/authController");
 
 const router = require("express").Router();
 
 router.route("/login").post(login);
-router.route("/logout").post(logout);
+router.route("/logout").post(authenticate, logout);
 
 module.exports = router;
